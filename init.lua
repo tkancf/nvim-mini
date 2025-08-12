4a2f5cdc65ebb10938fbb7bf1a2f8e8869a250b8
tkancf <18424095+tkancf@users.noreply.github.com>
2025-08-13 08:44:20 +0900

    Add some settings

--- init.lua
+++ init.lua
@@ -13,3 +13,41 @@ if not vim.loop.fs_stat(mini_path) then
   vim.cmd('packadd mini.nvim | helptags ALL')
   vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
+
+-- Basic Neovim settings
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.termguicolors = true
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+
+-- Leader key
+vim.g.mapleader = ' '
+
+-- Setup mini.nvim modules
+require('mini.statusline').setup()
+require('mini.tabline').setup()
+require('mini.files').setup()
+require('mini.completion').setup()
+require('mini.pairs').setup()
+require('mini.surround').setup()
+require('mini.comment').setup()
+require('mini.indentscope').setup()
+require('mini.starter').setup()
+
+-- Key mappings
+vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<cr>', { desc = 'Open file explorer' })
+vim.keymap.set('n', '<leader>f', '<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>', { desc = 'Open file explorer at current file' })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<cr>', { desc = 'Clear search highlights' })
+
+-- Terminal mappings
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
