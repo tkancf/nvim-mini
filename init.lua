1af089ed3b1ba675c1a9d9bfb51e7d67025367b1
tkancf <18424095+tkancf@users.noreply.github.com>
2025-08-16 14:56:37 +0900

    Add dial.nvim

--- init.lua
+++ init.lua
@@ -431,6 +431,56 @@ later(function()
       vim.cmd("AgendaViewWTF work companyA") -- Run the agenda view with tag filters
     end, {})
   end)
+
+  -- dial
+  later(function()
+    add('monaqa/dial.nvim')
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group({
+      default = {
+        augend.integer.alias.decimal, -- 整数（10進数）の増減
+        augend.integer.alias.hex,     -- 整数（16進数）の増減
+        augend.date.alias["%Y/%m/%d"], -- 日付の増減
+        augend.constant.alias.bool,  -- true/false のトグル
+        -- Default task cycle: TODO → WIP → DONE
+        augend.constant.new{
+          elements = {"TODO", "WIP", "DONE"},
+          word = true,
+          cyclic = false,
+        },
+        augend.constant.new{
+          elements = {"WAIT", "TODO"},
+          word = true,
+          cyclic = false,
+        },
+        augend.constant.new{
+          elements = {"NEXT", "TODO"},
+          word = true,
+          cyclic = false,
+        },
+        -- Schedule cycle: SCHE → DONE
+        augend.constant.new{
+          elements = {"SCHE", "DONE"},
+          word = true,
+          cyclic = false,
+        },
+        -- Schedule cycle: SCHE → DONE
+        augend.constant.new{
+          elements = {"- [ ] ", "- [x] "},
+          word = true,
+          cyclic = false,
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+    vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+    vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
+    vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
+    vim.keymap.set("v", "g<C-a>", require("dial.map").inc_gvisual(), { noremap = true })
+    vim.keymap.set("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
+  end)
+
 -- Key map
 vim.api.nvim_set_keymap('n', ':', ';', { noremap = true })
 vim.api.nvim_set_keymap('n', ';', ':', { noremap = true })
