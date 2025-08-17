8d75a9460fa3e1153a84d16455337d7adab47316
tkancf <18424095+tkancf@users.noreply.github.com>
2025-08-18 08:41:53 +0900

    Add autocmd to obisidian.nvim settings

--- init.lua
+++ init.lua
@@ -360,95 +360,106 @@ end)
 
 -- Obsidian.nvim
 now(function()
-  add('obsidian-nvim/obsidian.nvim')
-  local vault_path = "~/src/github.com/tkancf/tkancf.com/content/"
-  require("obsidian").setup {
-    legacy_commands = false,
-    ui = {
-      enable = false
-    },
-    attachments = {
-      img_folder = "assets", -- This is the default
-      img_text_func = function(client, path)
-        path = client:vault_relative_path(path) or path
-        return string.format("![%s](%s)", path.name, path)
-      end,
-    },
-    workspaces = {
-      {
-        name = "memo",
-        path = vault_path,
-      },
-    },
-    completion = {
-      nvim_cmp = true,
-      min_chars = 2,
-    },
-    preferred_link_style = "markdown",
-    daily_notes = {
-      folder = "",
-      date_format = "%Y-%m",
-      template = nil
-    },
-    picker = {
-      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
-      name = "mini.pick",
-      -- Optional, configure key mappings for the picker. These are the defaults.
-      -- Not all pickers support all mappings.
-      note_mappings = {
-        -- Create a new note from your query.
-        new = "<C-x>",
-        -- Insert a link to the selected note.
-        insert_link = "<C-l>",
-      },
-      tag_mappings = {
-        -- Add tag(s) to current note.
-        tag_note = "<C-x>",
-        -- Insert a tag at the current location.
-        insert_tag = "<C-l>",
-      },
-    },
-    ---@return string
-    note_id_func = function()
-      -- Generate a unique ID YYYYMMDDHHMMSS format
-      return "blog/" .. tostring(os.date("%Y%m%d%H%M%S"))
-    end,
-    ---@return table
-    note_frontmatter_func = function(note)
-      if note.title then
-        note:add_alias(note.title)
-      end
-      local created_time = os.date("%Y-%m-%d %H:%M") -- ISO 8601 format
-      local updated_time = created_time              -- Initially, created and updated times are the same
-      -- Initialize the frontmatter table
-      local out = {
-        id = note.id,
-        title = note.title,
-        aliases = note.aliases,
-        description = note.title,
-        tags = note.tags,
-        created = created_time,
-        updated = updated_time
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'markdown',
+    callback = function()
+      add({ 
+        source =  'obsidian-nvim/obsidian.nvim',
+        depends = {
+          'nvim-telescope/telescope.nvim',
+          'nvim-lua/plenary.nvim',
+
+        },
+      })
+      local vault_path = "~/src/github.com/tkancf/tkancf.com/content/"
+      require("obsidian").setup {
+        legacy_commands = false,
+        ui = {
+          enable = false
+        },
+        attachments = {
+          img_folder = "assets", -- This is the default
+          img_text_func = function(client, path)
+            path = client:vault_relative_path(path) or path
+            return string.format("![%s](%s)", path.name, path)
+          end,
+        },
+        workspaces = {
+          {
+            name = "memo",
+            path = vault_path,
+          },
+        },
+        completion = {
+          nvim_cmp = true,
+          min_chars = 2,
+        },
+        preferred_link_style = "markdown",
+        daily_notes = {
+          folder = "",
+          date_format = "%Y-%m",
+          template = nil
+        },
+        picker = {
+          -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+          name = "telescope.nvim",
+          -- Optional, configure key mappings for the picker. These are the defaults.
+          -- Not all pickers support all mappings.
+          note_mappings = {
+            -- Create a new note from your query.
+            new = "<C-x>",
+            -- Insert a link to the selected note.
+            insert_link = "<C-l>",
+          },
+          tag_mappings = {
+            -- Add tag(s) to current note.
+            tag_note = "<C-x>",
+            -- Insert a tag at the current location.
+            insert_tag = "<C-l>",
+          },
+        },
+        ---@return string
+        note_id_func = function()
+          -- Generate a unique ID YYYYMMDDHHMMSS format
+          return "blog/" .. tostring(os.date("%Y%m%d%H%M%S"))
+        end,
+        ---@return table
+        note_frontmatter_func = function(note)
+          if note.title then
+            note:add_alias(note.title)
+          end
+          local created_time = os.date("%Y-%m-%d %H:%M") -- ISO 8601 format
+          local updated_time = created_time              -- Initially, created and updated times are the same
+          -- Initialize the frontmatter table
+          local out = {
+            id = note.id,
+            title = note.title,
+            aliases = note.aliases,
+            description = note.title,
+            tags = note.tags,
+            created = created_time,
+            updated = updated_time
+          }
+
+          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
+            if note.metadata.created then out.created = note.metadata.created end
+            if note.metadata.updated then out.updated = note.metadata.updated end
+          end
+
+          return out
+        end,
+
+        ---@param url string
+        follow_url_func = function(url)
+          vim.fn.jobstart({ "open", url }) -- Mac OS
+          -- vim.fn.jobstart({"xdg-open", url})  -- linux
+        end,
       }
-
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
-        end
-        if note.metadata.created then out.created = note.metadata.created end
-        if note.metadata.updated then out.updated = note.metadata.updated end
-      end
-
-      return out
-    end,
-
-    ---@param url string
-    follow_url_func = function(url)
-      vim.fn.jobstart({ "open", url }) -- Mac OS
-      -- vim.fn.jobstart({"xdg-open", url})  -- linux
-    end,
-  }
-
+    end
+  })
   -- Key map
   vim.api.nvim_create_autocmd("User", {
     pattern = "ObsidianNoteEnter",
@@ -459,7 +470,6 @@ now(function()
       })
     end,
   })
-
 end)
 
 -- treesitter
