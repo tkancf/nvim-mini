94056770f6fc152fa56961a15723194a05ed38e9
tkancf <18424095+tkancf@users.noreply.github.com>
2025-08-18 08:42:51 +0900

    Update telescope keymap

--- init.lua
+++ init.lua
@@ -157,7 +157,7 @@ later(function()
   -- Using Lua functions
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-  vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
+  vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
   vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
 end)
 
