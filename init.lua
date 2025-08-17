319ad6f570d127d47899391df3f9d9b744812c11
tkancf <18424095+tkancf@users.noreply.github.com>
2025-08-18 08:40:24 +0900

    Add session management settings

--- init.lua
+++ init.lua
@@ -185,6 +185,60 @@ later(function()
 
   vim.keymap.set({ 'n', 'x' }, '<space>gs', MiniGit.show_at_cursor, { desc = 'Show at cursor' })
 end)
+
+-- session管理
+now(function()
+  require('mini.sessions').setup()
+
+  local function is_blank(arg)
+    return arg == nil or arg == ''
+  end
+  local function get_sessions(lead)
+    -- ref: https://qiita.com/delphinus/items/2c993527df40c9ebaea7
+    return vim
+    .iter(vim.fs.dir(MiniSessions.config.directory))
+    :map(function(v)
+      local name = vim.fs.basename(v)
+      return vim.startswith(name, lead) and name or nil
+    end)
+    :totable()
+  end
+  vim.api.nvim_create_user_command('SessionWrite', function(arg)
+    local session_name = is_blank(arg.args) and vim.v.this_session or arg.args
+    if is_blank(session_name) then
+      vim.notify('No session name specified', vim.log.levels.WARN)
+      return
+    end
+    vim.cmd('%argdelete')
+    MiniSessions.write(session_name)
+  end, { desc = 'Write session', nargs = '?', complete = get_sessions })
+
+  vim.api.nvim_create_user_command('SessionDelete', function(arg)
+    MiniSessions.select('delete', { force = arg.bang })
+  end, { desc = 'Delete session', bang = true })
+
+  vim.api.nvim_create_user_command('SessionLoad', function()
+    MiniSessions.select('read', { verbose = true })
+  end, { desc = 'Load session' })
+
+  vim.api.nvim_create_user_command('SessionEscape', function()
+    vim.v.this_session = ''
+  end, { desc = 'Escape session' })
+
+  vim.api.nvim_create_user_command('SessionReveal', function()
+    if is_blank(vim.v.this_session) then
+      vim.print('No session')
+      return
+    end
+    vim.print(vim.fs.basename(vim.v.this_session))
+  end, { desc = 'Reveal session' })
+end)
+
+-- 起動画面
+now(function()
+  require('mini.starter').setup()
+end)
+
 -- completion
 now(function()
   add('hrsh7th/nvim-cmp')
